9f84e73f0ef2b431de72c0b12b950e394aa0ec9f
CurajK <curajkristian123@gmail.com>
2026-04-01 13:06:06 +0200

    adasdas

---
+++ Wpf - PC-Koncorocny projekt/google/Google_domov.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf___PC_Koncorocny_projekt
+{
+    /// <summary>
+    /// Interaction logic for Google_domov.xaml
+    /// </summary>
+    public partial class Google_domov : Window
+    {
+        public Google_domov()
+        {
+            InitializeComponent();
+        }
+    }
+}
