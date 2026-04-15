800e78f13b618411199e5b17f0233924578c1262
Matej Kolkus <kolkus.m10@gmail.com>
2026-04-15 07:48:34 +0200

    posielam ti cast na robenie googlu.

--- Wpf - PC-Koncorocny projekt/google/Google_domov.xaml.cs
+++ Wpf - PC-Koncorocny projekt/google/Google_domov.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Security.Policy;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,6 +24,29 @@ namespace Wpf___PC_Koncorocny_projekt
         public Google_domov()
         {
             InitializeComponent();
+                       
+            this.Loaded += Google_domov_Loaded;
+        }
+
+        private async void Google_domov_Loaded(object? sender, RoutedEventArgs e)
+        {            
+            this.Loaded -= Google_domov_Loaded;
+
+            try
+            {
+                string query = "krionix";
+                string url = $"https://api.duckduckgo.com/?q={query}&format=json";
+
+                using HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("User-Agent", "DuckDuckGoSearch/1.0");
+                string response = await client.GetStringAsync(url);
+
+                System.Windows.MessageBox.Show(response);
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show($"Error fetching data: {ex.Message}");
+            }
         }
 
         private void SearchInput_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
@@ -43,11 +69,12 @@ namespace Wpf___PC_Koncorocny_projekt
 
         private void BtnCloseGoogle_Click(object sender, RoutedEventArgs e)
         {
-            // go back to home - close this window and show home
             var home = new WindowHome();
             home.Show();
             this.Close();
         }
+
+
 
     }
 }
